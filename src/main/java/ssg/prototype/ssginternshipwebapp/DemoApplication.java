f6b8e2db8b3308bf2ef764d1aa39512c3f77c16e
q80037 <q80037@EM0-W-D00914R.ssg.global>
2020-02-04 12:57:52 +0900

    mysql config, logger

--- src/main/java/ssg/prototype/ssginternshipwebapp/DemoApplication.java
+++ src/main/java/ssg/prototype/ssginternshipwebapp/DemoApplication.java
@@ -1,14 +1,17 @@
 package ssg.prototype.ssginternshipwebapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
-		// ø¿¡§¿± test
 		SpringApplication.run(DemoApplication.class, args);
+		logger.info("test log");
 	}
 
 }
