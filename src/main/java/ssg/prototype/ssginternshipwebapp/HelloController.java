b185b6f350eefe23f518da9518c9d76041ae31fa
홍승철/신입/q80041 <q80041@ssg.global>
2020-02-05 10:07:48 +0900

    following simple tutorials (in progress)

---
+++ src/main/java/ssg/prototype/ssginternshipwebapp/HelloController.java
@@ -0,0 +1,24 @@
+package ssg.prototype.ssginternshipwebapp;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	private static final String template = "Hello %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/")
+	public String index() {
+		return "greetings from spring boot";
+	}
+	
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
--- src/test/java/ssg/prototype/ssginternshipwebapp/DemoApplicationTests.java
+++ src/test/java/ssg/prototype/ssginternshipwebapp/DemoApplicationTests.java
@@ -1,13 +1,32 @@
 package ssg.prototype.ssginternshipwebapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+	
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("greetings from spring boot")));
+		
+		System.out.println("Test Completed!");
+		System.out.println("Initialize clean up...");
 	}
 
 }
