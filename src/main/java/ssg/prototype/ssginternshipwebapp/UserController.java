27178386d535c46d869d1f280207ad7c5f74c0e5
홍승철/신입/q80041 <q80041@ssg.global>
2020-02-06 13:25:35 +0900

    modifications made for displaying table of all users using jsp

--- src/main/java/ssg/prototype/ssginternshipwebapp/UserController.java
+++ src/main/java/ssg/prototype/ssginternshipwebapp/UserController.java
@@ -5,23 +5,26 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
 	@GetMapping("/users")
-	public ResponseEntity<List<User>> getAllUsers(){
-		return ResponseEntity.ok().body(userService.getAllUsers());
+	public String getAllUsers(Model model){
+		model.addAttribute("users", userService.getAllUsers());
+		return "user";
+		//return ResponseEntity.ok().body(userService.getAllUsers());
 	}
 	
 	@GetMapping("/users/{id}")
